1d560bf33da25dd0b58851bd07a6cd5333cc1e00
Jaber <j.boudouh.21500@stu.brucity.education>
2025-05-13 12:17:29 +0200

    test Django

--- reservation-frontend/src/App.js
+++ reservation-frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('Chargement...');
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/test/`)
+      .then((res) => setMessage(res.data.message))
+      .catch((err) => {
+        console.error('Erreur API :', err);
+        setMessage("Erreur d'appel API");
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test appel Django API</h1>
+      <p>{message}</p>
     </div>
   );
 }
